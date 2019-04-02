2b54a270e70997f9439c56b3a72064f88388c565
KushibikiMashu <kushibiki3413@gmail.com>
2019-04-02 21:10:32 +0900

    first commit

---
+++ stories/index.stories.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+
+import { Button, Welcome } from '@storybook/react/demo';
+
+import App from '../src/App'
+import Sample from '../src/Sample'
+
+storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')}/>);
+
+storiesOf('Button', module)
+  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+  .add('with some emoji', () => (
+    <Button onClick={action('clicked')}>
+      <span role="img" aria-label="so cool">
+        😀 😎 👍 💯
+      </span>
+    </Button>
+  ))
+  .add('sample', () => (
+    <Button onClick={action('SAMPLE_ACTION')}>
+      Fire SAMPLE_ACTION
+    </Button>
+  ));
+
+storiesOf('Create React App', module).add('index', () => <App/>);
+
+storiesOf('sample', module)
+  .add('sample component', () => <Button>sample button</Button>)
+    .add('sample text', () => <Sample/>);
