e921309266e96382ca76932e87ac0de28f7668e9
KushibikiMashu <kushibiki3413@gmail.com>
2019-04-05 10:55:25 +0900

    LINEのグループの詳細の配置

--- src/line_clone/App.js
+++ src/line_clone/App.js
@@ -9,15 +9,16 @@ export default function App() {
       <div className="Main">
         <div className="ChatRoom">
           <div className="ChatRoom--Img">
-            <img src={logo} width="100" alt="logo"/>
+            <img src={logo} alt="logo"/>
           </div>
           <div className="ChatRoom--Containts">
-            <div className="ChatRoom--Containts_Wrapper">
-            <p className="ChatRoom--Containts_RoomName">熊野鍋</p>
-            <p className="ChatRoom--Containts_Time">午前 11:18</p>
+            <div className="ChatRoom--Containts_Left">
+              <p className="ChatRoom--Containts_RoomName">熊野鍋 (7)</p>
+              <p className="ChatRoom--Containts_Description">おれ多分、7/6に上海行って、11日に西安でみんなと合流かな</p>
             </div>
-            <div className="ChatRoom--Containts_Description">
-              <p>おれ多分、7/6に上海行って、11日に西安でみんなと合流かな</p>
+            <div className="ChatRoom--Containts_Right">
+              <p className="ChatRoom--Containts_Time">午前 11:18</p>
+              <p className="ChatRoom--Containts_Badge">4</p>
             </div>
           </div>
         </div>
