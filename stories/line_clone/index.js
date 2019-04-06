de26ab409b3e39ec93651fa2496a2f56aab0cfdc
KushibikiMashu <kushibiki3413@gmail.com>
2019-04-07 00:28:03 +0900

    Storybookにチャットルームを追加

--- stories/line_clone/index.js
+++ stories/line_clone/index.js
@@ -2,6 +2,19 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 
 import App from '../../src/line_clone/App'
+import ChatRoom from '../../src/line_clone/ChatRoom'
+import reactLogo from "../../src/line_clone/react-logo.svg";
 
 storiesOf('LINE CLONE', module)
-  .add('App', () => <App/>);
+  .add('App', () => <App/>)
+  .add('ChatRoom', () => <ChatRoom {...props.react}/>)
+
+const props = {
+  "react": {
+    roomName: "React.js",
+    description: "React & StorybookでLINEのUIを模写する",
+    time: "午前 11:18",
+    newPostCount: "4",
+    img: reactLogo,
+  }
+}
