943a15757f6b91c1103766ccbfa8210028d5614a
KushibikiMashu <kushibiki3413@gmail.com>
2019-04-07 00:52:26 +0900

    サンプルデータをdata.jsに分離

--- src/line_clone/ChatRoomList.js
+++ src/line_clone/ChatRoomList.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ChatRoomItem from './ChatRoomItem'
-import reactLogo from "./react-logo.svg";
-import vueLogo from "./vue-logo.png";
-import angularLogo from "./angular-logo.svg";
+import { items } from './data.js'
 
 export default function ChatRoomList() {
   const chatRooms = items.map(item => <ChatRoomItem {...item}/>)
@@ -13,27 +11,3 @@ export default function ChatRoomList() {
     </>
   )
 }
-
-const items = [
-  {
-    roomName: "React.js",
-    description: "React & StorybookでLINEのUIを模写する",
-    time: "午前 11:18",
-    newPostCount: "4",
-    img: reactLogo
-  },
-  {
-    roomName: "Vue.js",
-    description: " Vue.jsのルームです",
-    time: "午前 7:08",
-    newPostCount: "1",
-    img: vueLogo
-  },
-  {
-    roomName: "AngularJS",
-    description: " AngularJSのルームです",
-    time: "昨日",
-    newPostCount: "0",
-    img: angularLogo
-  }
-]
---
+++ src/line_clone/data.js
@@ -0,0 +1,27 @@
+import reactLogo from './react-logo.svg'
+import vueLogo from './vue-logo.png'
+import angularLogo from './angular-logo.svg'
+
+export const items = [
+  {
+    roomName: "React.js",
+    description: "React & StorybookでLINEのUIを模写する",
+    time: "午前 11:18",
+    newPostCount: "4",
+    img: reactLogo
+  },
+  {
+    roomName: "Vue.js",
+    description: " Vue.jsのルームです",
+    time: "午前 7:08",
+    newPostCount: "1",
+    img: vueLogo
+  },
+  {
+    roomName: "AngularJS",
+    description: " AngularJSのルームです",
+    time: "昨日",
+    newPostCount: "0",
+    img: angularLogo
+  }
+]
