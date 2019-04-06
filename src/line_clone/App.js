80b1c540ebdd2e514fef3f5f94fedd5ff2972607
KushibikiMashu <kushibiki3413@gmail.com>
2019-04-07 00:23:36 +0900

    チャットルームを携帯アプリの見た目に近づける

--- src/line_clone/App.js
+++ src/line_clone/App.js
@@ -1,29 +1,10 @@
 import React from 'react';
 import './App.css';
+import ChatRoom from './ChatRoom'
 
 import reactLogo from './react-logo.svg';
 import vueLogo from './vue-logo.png';
 import angularLogo from './angular-logo.svg';
-
-function ChatRoom({roomName, description, time, newPostCount, img}) {
-  return (
-    <>
-      <div className="ChatRoom">
-        <div className="ChatRoom__Left">
-          <img className="ChatRoom__Image" src={img} alt="logo"/>
-        </div>
-        <div className="ChatRoom__Center">
-          <p className="ChatRoom__RoomName">{roomName}</p>
-          <p className="ChatRoom__Description">{description}</p>
-        </div>
-        <div className="ChatRoom__Right">
-          <p className="ChatRoom__Time">{time}</p>
-          {newPostCount !== "0" && <p className="ChatRoom__Badge">{newPostCount}</p>}
-        </div>
-      </div>
-    </>
-  )
-}
 
 export default function App() {
   const {react, vue, angular} = props
---
+++ src/line_clone/ChatRoom.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function ChatRoom({roomName, description, time, newPostCount, img}) {
+  return (
+    <>
+      <div className="ChatRoom">
+        <div className="ChatRoom__Left">
+          <img className="ChatRoom__Image" src={img} alt="logo"/>
+        </div>
+        <div className="ChatRoom__Center">
+          <p className="ChatRoom__RoomName">{roomName}</p>
+          <p className="ChatRoom__Description">{description}</p>
+        </div>
+        <div className="ChatRoom__Right">
+          <p className="ChatRoom__Time">{time}</p>
+          {newPostCount !== "0" && <p className="ChatRoom__Badge">{newPostCount}</p>}
+        </div>
+      </div>
+    </>
+  )
+}
