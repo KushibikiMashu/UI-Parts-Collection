ef16817951d9730b4f2680718e0196438b8455bf
KushibikiMashu <kushibiki3413@gmail.com>
2019-04-06 19:46:59 +0900

    チャットルームをコンポーネント化

--- src/line_clone/App.js
+++ src/line_clone/App.js
@@ -3,25 +3,39 @@ import './App.css';
 
 import logo from '../sample/logo.svg';
 
+function ChatRoom({roomName, description, time, newPostCount, img}) {
+  return (
+    <div className="ChatRoom">
+      <div className="ChatRoom--Img">
+        <img src={img} alt="logo"/>
+      </div>
+      <div className="ChatRoom--Containts">
+        <div className="ChatRoom--Containts_Left">
+          <p className="ChatRoom--Containts_RoomName">{roomName}</p>
+          <p className="ChatRoom--Containts_Description">{description}</p>
+        </div>
+        <div className="ChatRoom--Containts_Right">
+          <p className="ChatRoom--Containts_Time">{time}</p>
+          <p className="ChatRoom--Containts_Badge">{newPostCount}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
 export default function App() {
+  const props = {
+    roomName: "React (7)",
+    description: "React & StorybookでLINEのUIを模写する",
+    time:"午前 11:18",
+    newPostCount:"4",
+    img: logo,
+  }
   return (
     <>
       <div className="Main">
-        <div className="ChatRoom">
-          <div className="ChatRoom--Img">
-            <img src={logo} alt="logo"/>
-          </div>
-          <div className="ChatRoom--Containts">
-            <div className="ChatRoom--Containts_Left">
-              <p className="ChatRoom--Containts_RoomName">React (7)</p>
-              <p className="ChatRoom--Containts_Description">React & StorybookでLINEのUIを模写する</p>
-            </div>
-            <div className="ChatRoom--Containts_Right">
-              <p className="ChatRoom--Containts_Time">午前 11:18</p>
-              <p className="ChatRoom--Containts_Badge">4</p>
-            </div>
-          </div>
-        </div>
+        <ChatRoom {...props}/>
+        <ChatRoom {...props}/>
       </div>
     </>
   )
