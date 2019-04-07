14c5409b00ce997a07aff49ef1a7cb347704866a
KushibikiMashu <kushibiki3413@gmail.com>
2019-04-07 13:45:53 +0900

    送信ボタンを押すとメッセージを含むオブジェクトを作成する

--- src/line_clone/InputField.jsx
+++ src/line_clone/InputField.jsx
@@ -5,11 +5,29 @@ require("./InputField.scss");
 function InputField() {
     var _a = React.useState(''), text = _a[0], setText = _a[1];
     var inputCallback = React.useCallback(function (e) { return setText(e.target.value); }, [text]);
+    // userNameの取り出しにuseContextを使ってもいい
+    // useReducerを使う。
+    // keyPress Enterでもこの関数が動くようにする
+    function send() {
+        // const json = JSON.stringify({
+        //     userName: "self",
+        //     body: text,
+        //      time: (new Date()).toString()
+        // })
+        // console.log(json);
+        var obj = {
+            userName: "self",
+            body: text,
+            time: (new Date()).toString()
+        };
+        console.log(obj);
+        setText('');
+    }
     return (<div className="Input">
             <input className="Input__Field" type="text" value={text} onChange={inputCallback}/>
             <div className="Input__Icon">
                 {text ?
-        <i className="Input__Icon--Send material-icons">send</i> :
+        <i className="Input__Icon--Send material-icons" onClick={send}>send</i> :
         <i className="Input__Icon--Mic material-icons">mic</i>}
                 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
             </div>
--- src/line_clone/InputField.tsx
+++ src/line_clone/InputField.tsx
@@ -5,6 +5,25 @@ export default function InputField(): JSX.Element {
     const [text, setText] = React.useState('')
     const inputCallback = React.useCallback(e => setText(e.target.value), [text])
 
+    // userNameの取り出しにuseContextを使ってもいい
+    // useReducerを使う。
+    // keyPress Enterでもこの関数が動くようにする
+    function send() {
+        // const json = JSON.stringify({
+        //     userName: "self",
+        //     body: text,
+        //      time: (new Date()).toString()
+        // })
+        // console.log(json);
+        const obj = {
+            userName: "self",
+            body: text,
+            time: (new Date()).toString()
+        }
+        console.log(obj);
+        setText('')
+    }
+
     return (
         <div className="Input">
             <input
@@ -15,7 +34,7 @@ export default function InputField(): JSX.Element {
             />
             <div className="Input__Icon">
                 {text ?
-                    <i className="Input__Icon--Send material-icons">send</i> :
+                    <i className="Input__Icon--Send material-icons" onClick={send}>send</i> :
                     <i className="Input__Icon--Mic material-icons">mic</i>
                 }
                 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
