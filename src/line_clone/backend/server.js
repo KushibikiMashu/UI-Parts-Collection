7dd2d49e9757a23d671a7becb094cee4e68e23cb
KushibikiMashu <kushibiki3413@gmail.com>
2019-04-09 23:41:07 +0900

    CORSを許可

--- src/line_clone/backend/server.js
+++ src/line_clone/backend/server.js
@@ -7,13 +7,19 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
+// CORSを許可する
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.listen(3005)
 
 // GETでメッセージを取得
 app.get('/messages/list', (req, res) => {
   res.send('You got it.')
 })
-
 
 // POSTリクエスト
 // curl -X POST -H "Content-Type: application/json" -d '{"userName": "self", "body": "api test", "time": "today"}' 127.0.0.1:3005/messages
