0d29d9cdbfcf6f87cde0f1c5cee4ab451c1f92a1
KushibikiMashu <kushibiki3413@gmail.com>
2019-04-10 23:35:52 +0900

    Storybookでエラーのコンポーネントをモックに差し替え

--- stories/line_clone/index.js
+++ stories/line_clone/index.js
@@ -5,11 +5,10 @@ import App from '../../src/line_clone/frontend/components/App'
 import Room from '../../src/line_clone/frontend/components/Room'
 import RoomList from '../../src/line_clone/frontend/components/RoomList'
 import RoomItem from '../../src/line_clone/frontend/components/RoomItem'
-import MessageList from '../../src/line_clone/frontend/components/MessageList'
+// import MessageList from '../../src/line_clone/frontend/components/MessageList'
 import MyMessage from '../../src/line_clone/frontend/components/MyMessage'
 import OtherMessage from '../../src/line_clone/frontend/components/OtherMessage'
-import InputField from '../../src/line_clone/frontend/components/InputField'
-
+// import InputField from '../../src/line_clone/frontend/components/InputField'
 import { roomItems, messageItems } from '../../src/line_clone/frontend/data'
 
 storiesOf('LINE CLONE', module)
@@ -17,7 +16,60 @@ storiesOf('LINE CLONE', module)
   .add('Room', () => <Room/>)
   .add('RoomList', () => <>{roomItems.map(item => (<RoomItem {...item} key={item.id}/>))}</>)
   .add('RoomItem', () => <RoomItem {...roomItems[0]}/>)
-  // .add('MessageList', () => 'Error because of no useContext initialization')
+  .add('MessageListMock', () => <MessageListMock/>)
   .add('MyMessage', () => <MyMessage {...messageItems[3]}/>)
   .add('OtherMessage', () => <OtherMessage {...messageItems[0]}/>)
-  // .add('InputField', () => 'Error because of no useContext initialization')
+  .add('InputField', () => <InputFieldMock/>)
+
+
+function MessageListMock() {
+  return <>
+    {messageItems.map(
+      item => {
+        return item.userName === 'self' ?
+          <MyMessage {...item} key={item.id}/> :
+          <OtherMessage {...item} key={item.id}/>
+      })}
+  </>
+}
+
+function InputFieldMock() {
+  const [text, setText] = React.useState('')
+  const [clicked, watchClicked] = React.useState(false)
+  const setTextCallback = React.useCallback(e => setText(e.target.value), [text])
+
+  React.useEffect(() => {
+    const message = text.trim()
+    if (message === '') {
+      setText('')
+      return
+    }
+    setText('')
+  }, [clicked])
+
+  return (
+    <div className="Input">
+      <input
+        className="Input__Field"
+        type="text"
+        value={text}
+        onChange={setTextCallback}
+        onKeyPress={e => {
+          if (e.key === 'Enter') {
+            watchClicked(!clicked)
+          }
+        }}
+      />
+      <div className="Input__Icon">
+        {text ?
+          <i
+            className="Input__Icon--Send material-icons"
+            onClick={() => watchClicked(!clicked)}
+          >send</i> :
+          <i className="Input__Icon--Mic material-icons">mic</i>
+        }
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
+      </div>
+    </div>
+  )
+}
