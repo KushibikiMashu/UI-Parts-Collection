27d396c79c54cb18e8a70274d9a5830acc6ff74e
KushibikiMashu <kushibiki3413@gmail.com>
2019-04-10 19:45:03 +0900

    Storybookで表示させるコンポーネントを選別

--- stories/line_clone/index.js
+++ stories/line_clone/index.js
@@ -11,32 +11,13 @@ import Message from '../../src/line_clone/frontend/components/OtherMessage'
 import InputField from '../../src/line_clone/frontend/components/InputField'
 
 import { roomItems, messageItems } from '../../src/line_clone/frontend/data'
-import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 
 storiesOf('LINE CLONE', module)
   .add('App', () => <App/>)
   .add('Room', () => <Room/>)
   .add('RoomList', () => <RoomList/>)
   .add('RoomItem', () => <RoomItem {...roomItems[0]}/>)
-  .add('MessageList', () => 'Error because of no useContext initialization')
+  // .add('MessageList', () => 'Error because of no useContext initialization')
   .add('MyMessage', () => <MyMessage {...messageItems[3]}/>)
   .add('Message', () => <Message {...messageItems[0]}/>)
-  .add('InputField', () => 'Error because of no useContext initialization')
-  .add('test', () => {
-    return <>
-      <Router>
-        <div className="Main">
-          <ul>
-            <li>
-              <Link to="/">List</Link>
-            </li>
-            <li>
-              <Link to="/room">Room</Link>
-            </li>
-          </ul>
-          <Route path="/" exact component={RoomList}/>
-          <Route path="/room" component={Room}/>
-        </div>
-      </Router>
-      </>
-  })
+  // .add('InputField', () => 'Error because of no useContext initialization')
